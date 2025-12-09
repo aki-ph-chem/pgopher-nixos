bf0bae7d29c0a79bb49bf35ed7a16b73e306b89d
aki <lmxox@proton.me>
2025-12-09 10:46:11 +0900

    flake for pgopher package => ok

--- flake.nix
+++ flake.nix
@@ -10,12 +10,64 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      pkg = pkgs.stdenv.mkDerivation {
+        pname = "pgopher";
+        version = "1.0";
+
+        src = pkgs.fetchurl {
+          url = "https://github.com/aki-ph-chem/pgopher-nixos/releases/download/v0/pgopher-x86_64-linux-gtk2.tgz";
+          sha256 = "sha256-pIgHTazLmWN+gPPTMs6yIwSSO9SgLSqguR3VFm6m/5E=";
+        };
+
+        # needed by build this package
+        nativeBuildInputs = [
+          pkgs.gnutar
+          pkgs.makeWrapper
+        ];
+
+        unpackPhase = ''
+          tar -xzf "$src"
+        '';
+
+        installPhase = ''
+          # prepare directory
+          mkdir -p "$out/bin"
+          mkdir -p "$out/share/applications"
+          mkdir -p "$out/share/icons/hicolor/64x64/apps"
+
+          # executable binary
+          cp pgopher "$out/bin/"
+          cp pgo "$out/bin/"
+          cp tabslave "$out/bin/"
+
+          # desktop entry & icon file
+          cp ${./pgopher.desktop} "$out/share/applications/"
+          cp ${./pgopher.gif} "$out/share/icons/hicolor/64x64/apps/"
+        '';
+
+      };
     in
     {
-      src = pkgs.fetchurl {
-        url = "https://github.com/aki-ph-chem/pgopher-nixos/releases/download/v0/pgopher-x86_64-linux-gtk2.tgz";
-        sha256 = "sha256-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX=";
-      };
+      packages.${system}.default = pkg;
 
+      apps.${system} = {
+        pgopher = {
+          type = "app";
+          program = "${pkg}/bin/pgopher";
+        };
+        pgo = {
+          type = "app";
+          program = "${pkg}/bin/pgo";
+        };
+        tabslave = {
+          type = "app";
+          program = "${pkg}/bin/tabslave";
+        };
+
+        default = {
+          type = "app";
+          program = "${pkg}/bin/pgopher";
+        };
+      };
     };
 }
